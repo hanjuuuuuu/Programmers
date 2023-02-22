6e8658261019498305ea080a9ecf084b131e3205
hanjuuuuuu <hanjucomss@naver.com>
2023-02-22 22:42:07 +0900

    달 더하는 함수 추가

--- 개인정보 수집 유효기간/app.js
+++ 개인정보 수집 유효기간/app.js
@@ -1,7 +1,22 @@
 //개인정보 수집 일자 + 유효기간 vs 오늘 (오늘이 더 이전날짜면 파기)
 //오늘-개인정보 수집 일자 vs 유효기간 (유효기간이 더 작으면 파기)
+const addMonth = (date, month) => {
+    let [year, mon, day] = date.split('.').map((x) => Number(x));
+    mon += month;
+    day -= 1;
+    if(day === 0){
+        day = 28;
+        mon -= 1;
+    }
+    if(mon > 12){
+        mon -= 1;
+        year += 1;
+    }
+    return [year, mon, day];
+}
 
 function solution(today, terms, privacies) {
+
     var answer = [];
     for(let i=0; i<privacies.length; i++){
         var collectdate = privacies[i].split(' ')
