a6080731aab475a09362930d1fa3d1afbca0f3f1
hanjuuuuuu <hanjucomss@naver.com>
2023-02-27 23:13:00 +0900

    for문으로 해결

--- 푸드 파이트 대회/app.js
+++ 푸드 파이트 대회/app.js
@@ -1,10 +1,15 @@
 function solution(food) {
-    var answer = '';
+    var answer = [];
     for(let i=1; i<food.length; i++){
         let count = Math.floor(food[i]/2)
-        for(let j=0; j<count; j++)
-            answer += i
+        for(let j=0; j<count; j++){
+            answer.push(i);
+        } 
     }
-    answer += 0;
-    return answer;
+    answer.push(0);
+    for(let j=answer.length-2; j>=0; j--)
+        answer.push(answer[j])
+    return answer.join('');
 }
+
+solution([1,3,4,6])
