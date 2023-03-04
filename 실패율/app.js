e28cde37ccd61e130977e475c78505144f77c489
hanjuuuuuu <hanjucomss@naver.com>
2023-03-04 23:35:28 +0900

    sort와 map써서 해결

--- 실패율/app.js
+++ 실패율/app.js
@@ -1,17 +1,29 @@
 function solution(N, stages) {
     var answer = [];
     var failure = 0;
-    var player = 0;
+    var player = stages.length;
+
     for(let i=1; i<=N; i++){
         var count = 0;
         for(let j=0; j<stages.length; j++){
             if(stages[j] === i){
                 count++;
-                stages.splice(j,1)
             }
         }
+        player -= failure;
         failure = count;
-        player = stages.length
+        rate = failure/player;
+        answer.push({stage:i,rate:rate});
     }
-    return answer;
+
+    answer.sort((a,b) => {
+        if(a.rate === b.rate)
+            return a.stage-b.stage;
+        else
+            return b.rate-a.rate;
+    });
+
+    return answer.map(item=>item.stage)
 }
+
+solution(5, [2, 1, 2, 6, 2, 4, 3, 3])
