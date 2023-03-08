d0b75ab319e1fecc57fbc81fe7a6a0e37860412d
hanjuuuuuu <hanjucomss@naver.com>
2023-03-08 23:06:16 +0900

    수정 완료

--- 체육복/app.js
+++ 체육복/app.js
@@ -1,9 +1,11 @@
 function solution(n, lost, reserve) {
-    let answer = n - lost.length;
-
     //중복 제거
     let realLost = lost.filter(x => !reserve.includes(x));        //체육복이 한 벌도 없는 사람
     let realReserve = reserve.filter(x => !lost.includes(x));       //여분 체육복이 있는 사람
+
+    let answer = n - realLost.length;
+    
+    //정렬
     realLost.sort((a,b) => a-b);
     realReserve.sort((a,b)=> a-b);
     
