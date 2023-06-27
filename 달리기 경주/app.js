61f1a50a823c0c7da1ae0cd3b98dfe621c67c326
hanjuuuuuu <hanjucomss@naver.com>
2023-06-27 11:41:52 +0900

    배열에서 객체로 변경해서 해결

--- 달리기 경주/app.js
+++ 달리기 경주/app.js
@@ -1,9 +1,22 @@
-//인덱스를 찾아서 변경 --> 시간초과
 function solution(players, callings) {
-    for (let i=0; i<callings.length; i++){
-        let indexnum = players.indexOf(callings[i])
-        players[indexnum] = players[indexnum-1];
-        players[indexnum-1] = callings[i];
+    var answer = {};
+    
+    //객체로 plqyers의 인덱스 값 저장({"mumu":0, "soe":1})
+    for(let i=0; i<players.length; i++){
+        answer[players[i]] = i;
     }
+    
+    //추월한 선수 자리 바꾸기
+    callings.forEach(ele => {
+        let indexnum = answer[ele]; 
+        let frontname = players[indexnum-1]; 
+        
+        players[indexnum] = frontname;
+        players[indexnum-1] = ele;
+        
+        //객체도 변경 ({ mumu: 0, soe: 1, poe: 2, kai: 3, mine: 4 })
+        answer[ele]--;
+        answer[frontname]++;
+    } )
     return players;
 }
